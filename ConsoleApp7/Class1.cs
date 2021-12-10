270b29b94745d581a8283446ffbcd7404f446a69
Joshua Brandt <joshua.a.brandt@outlook.com>
2021-12-10 13:15:15 -0600

    Add project files.

---
+++ ConsoleApp7/Class1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    public class Change
+    {
+        int quarters;
+        int dimes;
+        int nickles;
+        int pennies;
+
+       public Change(int changeConvert)
+        {
+
+            string changetypes = CalculateLeastChange(changeConvert);
+            Console.WriteLine(changetypes);
+        }
+
+        public string CalculateLeastChange(int changeToConvert)
+        {
+            bool validInput;
+            quarters = changeToConvert / 25;
+            validInput = GreaterThanZero(quarters);
+            if (validInput == true)
+            {
+                changeToConvert = changeToConvert - (quarters * 25);
+            }
+            dimes = changeToConvert / 10;
+            validInput = GreaterThanZero(dimes);
+            if (validInput == true)
+            {
+                changeToConvert = changeToConvert - (dimes * 10);
+            }
+            nickles = changeToConvert / 5;
+            validInput = GreaterThanZero(nickles);
+            if (validInput == true)
+            {
+                changeToConvert = changeToConvert - (nickles * 5);
+            }
+
+            string amountOfCoins = quarters + " quarters " + dimes + " dimes " + nickles + " nickles " + changeToConvert + " pennies";
+            return amountOfCoins;
+
+        }
+
+        public void CalculateAllChange(int changeAmount)
+        {
+
+
+
+
+
+        }
+
+        public bool GreaterThanZero(int checkZero)
+        {
+            bool greaterThanZero;
+            if (checkZero > 0)
+            {
+                greaterThanZero = true;
+            }
+            else
+            {
+                greaterThanZero = false;
+            }
+
+            return greaterThanZero;
+        }
+
+
+    }
+}
---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Collections;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int changeInput = int.Parse(Console.ReadLine());
+            Change myChange = new Change(changeInput);
+
+
+
+
+        }
+    
+    }
+}
